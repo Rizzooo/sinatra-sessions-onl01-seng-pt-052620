ded73ebfa862ed4514bbc9dd83d5aea889ad4452
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-08-25 18:17:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -32,6 +32,11 @@ class App < Sinatra::Base
 
   get '/set_session' do
     session[:id] = 1
+      # "Session ID set. It's currently set to #{session[:id]}."
+      redirect '/fetch_session_id'
+    else
+      "Session ID has not been set!"
+    end
   end
 
   get '/fetch_session_id' do
